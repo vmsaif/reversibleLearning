d76814a1fcb0b2154c412e3566c8133b935ec7fd
aliahses <aliahses@myumanitoba.ca>
2021-02-16 10:18:07 +0500

    Created this class which will work to create an options activity presentation

---
+++ app/src/main/java/presentation/OptionsActivity.java
@@ -0,0 +1,14 @@
+package presentation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import flashcard.group5.application.R;
+
+public class OptionsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashcard);
+    }//onCreate
+}//OptionsActivity
