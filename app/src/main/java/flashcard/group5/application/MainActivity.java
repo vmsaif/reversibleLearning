47de64a32d505e6a606e063a1a927cfaf7e2a66e
Saif Mahmud <60409889+saif81bd@users.noreply.github.com>
2021-02-16 17:21:03 -0600

    renamed button id

--- app/src/main/java/flashcard/group5/application/MainActivity.java
+++ app/src/main/java/flashcard/group5/application/MainActivity.java
@@ -14,14 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button optionsButton = findViewById(R.id.button_options);
-        optionsButton.setOnClickListener(new View.OnClickListener() {
+
+        //this is button 1 option
+        Button guestLogin = findViewById(R.id.button_guestLogin);
+        guestLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openOptionsActivity();
             }//onClick
         });
     }//onCreate
+
 
     public void openOptionsActivity(){
         Intent intent_options = new Intent(getApplicationContext(), OptionsActivity.class);
