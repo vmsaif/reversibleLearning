763b604cccabbdc7bf384e1e2818bd893c70689f
LizaYa <liza.ya3011@gmail.com>
2021-02-25 00:15:41 -0600

    User database + add users

--- app/src/main/java/interfaces/IUser.java
+++ app/src/main/java/interfaces/IUser.java
@@ -2,12 +2,12 @@ package interfaces;
 
 public interface IProfile {
 
-    void changefName(String first_name);
-
-    void changelName(String last_name);
-
     void changeUserName(String user_name);
 
     void changePassword(String password);
+
+     String getUserName();
+
+     String getPassword();
 
 }
--- app/src/main/java/objects/User.java
+++ app/src/main/java/objects/User.java
@@ -4,26 +4,15 @@ import interfaces.IProfile;
 public class Profile implements IProfile{
 
     //variables
-    private String first_name;
-    private String last_name;
     private String user_name;
     private String password;
 
     //constructor
-    public Profile(String first_name, String last_name, String user_name, String password){
-        this.first_name = first_name;
-        this.last_name = last_name;
+    public Profile(String user_name, String password){
         this.user_name = user_name;
         this.password = password;
     }
 
-    public void changefName(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public void changelName(String last_name) {
-        this.last_name = last_name;
-    }
 
     public void changeUserName(String user_name) {
         this.user_name = user_name;
@@ -32,4 +21,8 @@ public class Profile implements IProfile{
     public void changePassword(String password) {
         this.password = password;
     }
+
+    public String getUserName(){ return this.user_name; }
+
+    public String getPassword() { return this.password; }
 }
