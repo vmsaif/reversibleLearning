ef7a1da0d41a5d8bc851ec7df6383760a9641687
Saif Mahmud <mahmuds3@myumanitoba.ca>
2021-02-25 17:58:09 -0600

    Revert "Fixes #47"
    This reverts commit 7bb49ee41c74056b6b6237cfe93a93870a2fca08

--- app/src/main/java/data/LoginDataSource.java
+++ app/src/main/java/data/LoginDataSource.java
@@ -1,24 +1,29 @@
 package data;
 
-import data.model.LoggedInUser;
-import interfaces.ILoginDataSource;
+import flashcard.group5.application.MainActivity;
+import objects.User;
 
 import java.io.IOException;
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
-public class LoginDataSource implements ILoginDataSource {
+public class LoginDataSource {
 
-    public Result<LoggedInUser> login(String username, String password) {
-
+    public Result<User> login(String username, String password) {
         try {
+            UserDB userDB = MainActivity.getUserDB();
+            User currUser = new User(username, password);
+
+            if(userDB.userExits(currUser))
+
             // TODO: handle loggedInUser authentication
-            LoggedInUser fakeUser =
+         /*   LoggedInUser fakeUser =
                     new LoggedInUser(
                             java.util.UUID.randomUUID().toString(),
-                            "Jane Doe");
-            return new Result.Success<>(fakeUser);
+                            "Jane Doe");*/
+            return new Result.Success<>(currUser);
+            else return new Result.Error(new IOException("User not found"));
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
         }
