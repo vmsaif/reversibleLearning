b3443b1c02f9f06ff6b0dc63e7a6070441bc59d8
Liza Yashin <yashiny@myumanitoba.ca>
2021-02-26 14:27:01 -0600

    Add restrictions to the register page fields

--- app/src/main/java/presentation/ProfileCreationView.java
+++ app/src/main/java/presentation/ProfileCreationView.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,16 +34,38 @@ public class ProfileCreationView extends AppCompatActivity {
             register.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    UserDB userDB = MainActivity.getUserDB();
-                    User currUser = new User(usernameEditText.getText().toString(), passwordEditText.getText().toString());
-                    userDB.addUser(currUser);
-                    MainActivity.updateUserDB(userDB);
-                    Intent intent = new Intent(ProfileCreationView.this, LoginActivity.class);
-                    startActivity(intent);
+                    if(!isUserNameValid(usernameEditText.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Please enter a valid user name", Toast.LENGTH_SHORT).show();
+
+                    else if(!isPasswordValid(passwordEditText.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Please enter a valid password. Password must be > 5.", Toast.LENGTH_SHORT).show();
+
+                    else {
+                        UserDB userDB = MainActivity.getUserDB();
+                        User currUser = new User(usernameEditText.getText().toString(), passwordEditText.getText().toString());
+                        userDB.addUser(currUser);
+                        MainActivity.updateUserDB(userDB);
+                        Intent intent = new Intent(ProfileCreationView.this, LoginActivity.class);
+                        startActivity(intent);
+                    }
                 }
             });
 
-
         }
+    private boolean isUserNameValid(String username) {
+        if (username == null) {
+            return false;
+        }
+        if (username.contains("@")) {
+            return Patterns.EMAIL_ADDRESS.matcher(username).matches();
+        } else {
+            return !username.trim().isEmpty();
+        }
+    }
+
+    // A placeholder password validation check
+    private boolean isPasswordValid(String password) {
+        return password != null && password.trim().length() > 5;
+    }
 
 }
