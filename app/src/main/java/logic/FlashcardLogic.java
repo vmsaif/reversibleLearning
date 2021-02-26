7dca224614e285a39675a8405615dc6ada5d9026
aliahses <aliahses@myumanitoba.ca>
2021-02-26 14:11:35 -0600

    Re did the animation and removed unused code. Also nested the cardview class inside flashcard class

--- app/src/main/java/logic/FlashcardLogic.java
+++ app/src/main/java/logic/FlashcardLogic.java
@@ -1,6 +1,34 @@
 package logic;
 
-public class FlashcardLogic {
-    //Will call functions from flashcard class to implement the logic which will be used to implement the GUI
-    //new text
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import objects.Flashcard;
+import presentation.FlashcardActivity;
+import presentation.CardviewActivity;
+
+public class FlashcardLogic{
+
+    //variables
+    Flashcard fCard;
+
+    //constructor
+    public FlashcardLogic(String question, String answer){
+        fCard = new Flashcard();
+        fCard.modifyQuestion(question);
+        fCard.modifyAnswer(answer);
+    }//constructor
+
+
+    //getQuestion---calls the returnQuestion method
+    public String getQuestion(){
+        return fCard.returnQuestion();
+    }//getQuestion
+
+
+    //getAnswer---calls the returnAnswer method
+    public String getAnswer(){
+        return fCard.returnAnswer();
+    }//getAnswer
+
 }//FlashcardLogic class
--- app/src/main/java/presentation/CardviewActivity.java
+++ app/src/main/java/presentation/CardviewActivity.java
@@ -2,57 +2,55 @@ package presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
 import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.TextView;
 
 import flashcard.group5.application.R;
+import logic.FlashcardLogic;
 
 public class CardviewActivity extends AppCompatActivity {
 
     //variables
-    private AnimatorSet front_anim;
-    private AnimatorSet back_anim;
-    private boolean isFront = true;
-    private Button flipButton;
-    private TextView front_t;
-    private TextView back_t;
+    private ObjectAnimator anime_1;
+    private ObjectAnimator anime_2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardview);
-        float scale = getApplicationContext().getResources().getDisplayMetrics().density;
 
-        front_t = (TextView) findViewById(R.id.card_front);
-        back_t = (TextView) findViewById(R.id.card_back);
-        front_t.setCameraDistance(8000 * scale);
-        back_t.setCameraDistance(8000 * scale);
+        TextView front_card = (TextView) findViewById(R.id.card_front);
 
-        front_anim = (AnimatorSet) AnimatorInflater.loadAnimator(getApplicationContext(),R.animator.front_animator);
-        back_anim = (AnimatorSet) AnimatorInflater.loadAnimator(getApplicationContext(),R.animator.back_animator);
-
-        flipButton = (Button) findViewById(R.id.button_flip);
-        flipButton.setOnClickListener(new View.OnClickListener() {
+        front_card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (isFront){
-                    front_anim.setTarget(front_t);
-                    back_anim.setTarget(back_t);
-                    front_anim.start();
-                    back_anim.start();
-                    boolean isFront = false;
-                }//if
-                else {
-                    front_anim.setTarget(front_t);
-                    back_anim.setTarget(back_t);
-                    back_anim.start();
-                    front_anim.start();
-                    boolean isFront = true;
-                }//else
+                anime_1 = ObjectAnimator.ofFloat(front_card, "scaleX", 1f, 0f);//first animation will shrink to reveal the back side
+                anime_2 = ObjectAnimator.ofFloat(front_card, "scaleX", 0f, 1f);//reverse of the previous line
+                anime_1.setInterpolator(new DecelerateInterpolator());//reducing speed of animation at the start while flipping
+                anime_2.setInterpolator(new AccelerateInterpolator());//bringing the speed back ti normal
+
+                anime_1.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        front_card.setText("Answer"); //placeholder text reveals the answer...real implementation will be done once actual database is implemented
+                        anime_2.start();
+                    }
+                });//AnimatorListenerAdapter
+                anime_1.start();
             }//onClick
         });
 
