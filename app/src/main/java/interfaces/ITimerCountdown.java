d250f70197e9e6ecae1fe8fbd0b4c26bf8c826cc
paxx dream <warsitol@myumanitoba.ca>
2021-03-07 16:46:35 -0600

    modify the comment

--- app/src/main/java/interfaces/ITimerCountdown.java
+++ app/src/main/java/interfaces/ITimerCountdown.java
@@ -10,7 +10,7 @@ public interface ITimerCountdown {
     // start the timer
     CountDownTimer startTimer();
 
-    // update the time left every second
+    // update the time left every second locally
     void updateTimer();
 
     // get the current time left in the format of String
