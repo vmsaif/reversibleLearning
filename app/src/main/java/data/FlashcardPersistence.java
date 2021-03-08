1d6394cbd8b572f82ed7242c14531d751297def2
aliahses <aliahses@myumanitoba.ca>
2021-03-07 21:42:42 -0600

    -Created a database for Flashcard objects
    -added user name and folder name attributes to Flashcard object with their getters and setter (both in the interface and java class)

---
+++ app/src/main/java/data/FlashcardPersistence.java
@@ -0,0 +1,18 @@
+package data;
+
+import java.util.List;
+
+import objects.Flashcard;
+
+public interface FlashcardPersistence {
+
+    //getFlashcardSequential---returns the flashcards in the sequence stored in the table
+    List<Flashcard> getFlashcardSequential();
+
+    //insertFlashcard---inserts a flashcard in this database
+    void insertFlashcard(Flashcard givenFlashcard);
+
+    //deleteFlashcard---deletes a flashcard in this database
+    void deleteFlashcard(Flashcard currentFlashcard);
+
+}//FlashcardPersistence ends
