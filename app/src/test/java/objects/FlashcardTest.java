950c8c27c54113d4860078543ace9c37157fde41
paxx dream <warsitol@myumanitoba.ca>
2021-03-13 09:23:24 -0600

    changed the test to satisfy the new flashcard object

--- app/src/test/java/objects/FlashcardTest.java
+++ app/src/test/java/objects/FlashcardTest.java
@@ -3,11 +3,13 @@ package objects;
 import org.junit.Before;
 import org.junit.Test;
 
+import interfaces.IFlashcard;
+
 import static org.junit.Assert.*;
 
 public class FlashcardTest {
     //Variable
-    private Flashcard fCard;
+    private IFlashcard fCard;
 
     @Before
     public void setUp(){
@@ -15,20 +17,27 @@ public class FlashcardTest {
     }
 
     @Test
+    public void TestInitialization() {
+        //Tests the constructor
+        IFlashcard expected = new Flashcard("Question", "Answer", "Ali");
+        assertTrue("The question should be the same",expected.getQuestion().equals(fCard.getQuestion()));
+        assertTrue("The answer should be the same",expected.getAnswer().equals(fCard.getAnswer()));
+    }
+
+    @Test
     public void TestModifyQuestionAndReturnQuestion() {
         //Tests the methods modifyQuestion and returnQuestion
-        Flashcard expected = new Flashcard("Q", "A", "");
+        IFlashcard expected = new Flashcard("Q", "A", "");
         expected.modifyQuestion("This is the question");
         fCard.modifyQuestion("This is the question");
-        assertTrue("Modified and returned question should be same",expected.getQuestion().equals(fCard.getQuestion()));//Modify the question again and test it
-
+        assertTrue("Modified and returned question should be same",expected.getQuestion().equals(fCard.getQuestion()));//Modify the question  and test it
     }
 
 
     @Test
     public void TestModifyAnswerAndReturnAnswer() {
         //Tests the method modifyAnswer and getAnswer
-        Flashcard expected = new Flashcard("Q", "A", "");
+        IFlashcard expected = new Flashcard("Q", "A", "");
         expected.modifyAnswer("This is the answer");
         fCard.modifyAnswer("This is the answer");
         assertTrue("Modified and returned answer should be same",expected.getAnswer().equals(fCard.getAnswer()));
