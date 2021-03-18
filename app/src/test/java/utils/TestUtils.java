4d42d08ba9dd5cc571341337c74a465485433596
paxx dream <warsitol@myumanitoba.ca>
2021-03-17 23:27:54 -0500

    added integration test

--- app/src/test/java/utils/TestUtils.java
+++ app/src/test/java/utils/TestUtils.java
@@ -1,4 +1,19 @@
 package utils;
 
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.io.IOException;
+
+import flashcard.group5.application.Services;
+
 public class TestUtils {
+    private static final File DB_SRC = new File("src/main/assets/db/DB.script");
+
+    public static File copyDB() throws IOException {
+        final File target = File.createTempFile("temp-db", ".script");
+        Files.copy(DB_SRC, target);
+        Services.setDBPathName(target.getAbsolutePath().replace(".script", ""));
+        return target;
+    }
 }
