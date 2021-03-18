227170bd58306af22caa8f1c4ae14ebe7a83973e
paxx dream <warsitol@myumanitoba.ca>
2021-03-18 17:34:29 -0500

    corrected the filename

--- app/src/test/java/UnitTest.java
+++ app/src/test/java/UnitTest.java
@@ -1,17 +1,26 @@
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import Logic.AccountIT;
-import Logic.FlashcardLogicIT;
+import data.FlashcardPersistenceTest;
+import data.UserDBTest;
+import data.UserPersistenceTest;
+import objects.FlashcardListTest;
+import objects.FlashcardTest;
+import objects.UserTest;
 
 // import all the test files
 
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        // the integration tests
-        AccountIT.class,
-        FlashcardLogicIT.class,
+        // the object classes
+        FlashcardListTest.class,
+        UserTest.class,
+        FlashcardTest.class,
+        // the data classes
+        UserPersistenceTest.class,
+        FlashcardPersistenceTest.class,
+        UserDBTest.class,
 })
 
 
