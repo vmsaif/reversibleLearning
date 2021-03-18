87f6126a5a16d68bfcb5bb6e376e26f46aa8d4a7
Saif Mahmud <60409889+saif81bd@users.noreply.github.com>
2021-03-18 18:50:44 -0500

    closes #66 Profile Activity(.xml) without the connection to the logic layer(ProfileActivity.java).

--- app/src/main/java/presentation/OptionsActivity.java
+++ app/src/main/java/presentation/OptionsActivity.java
@@ -15,12 +15,27 @@ public class OptionsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
         RelativeLayout createFlashCardWidget = findViewById(R.id.makeFlashCard); //this button will take you to the flashcard activity where users will be able to make a flashcard
         createFlashCardWidget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { openFlashcardActivity(); }//onClick
         });
+
+        RelativeLayout profileWidget = findViewById(R.id.profileActivity);
+        profileWidget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openProfileActivity();
+            }
+        });
+
     }//onCreate
+
+    public void openProfileActivity() {
+        Intent intent_flashcard = new Intent(this, ProfileActivity.class);
+        startActivity(intent_flashcard);
+    }
 
     public void openFlashcardActivity(){
         Intent intent_flashcard = new Intent(this, FlashcardActivity.class);
---
+++ app/src/main/java/presentation/ProfileActivity.java
@@ -0,0 +1,15 @@
+package presentation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import flashcard.group5.application.R;
+import logic.Account;
+
+public class ProfileActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
