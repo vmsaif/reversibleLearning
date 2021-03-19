ec934713b766ccb2d8b03dab2cf6839c99db7d34
LizaYa <liza.ya3011@gmail.com>
2021-03-18 23:18:46 -0500

    Linking change info UI and backend

--- app/src/main/java/presentation/ProfileActivity.java
+++ app/src/main/java/presentation/ProfileActivity.java
@@ -1,8 +1,13 @@
 package presentation;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,11 +16,15 @@ import com.google.android.material.textfield.TextInputLayout;
 
 import flashcard.group5.application.R;
 import logic.Account;
+import logic.AccountValidator;
+import objects.User;
 
 public class ProfileActivity extends AppCompatActivity {
 
     TextInputLayout userName, password;
     MaterialButton updateButton;
+    Account account;
+    String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +35,29 @@ public class ProfileActivity extends AppCompatActivity {
         password = findViewById(R.id.profilePasswordField);
         updateButton = findViewById(R.id.profileUpdateButton);
 
+        account = new Account();
+
         updateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // button things
-            }
+                    message = AccountValidator.validatePassword(password.toString());
+                    message = AccountValidator.validateUserName(userName.toString());
+
+                    if(message == null) {
+                        if (account.changeUser(new User(userName.toString(), password.toString()))) {
+                            Toast.makeText(getApplicationContext(), "Your information has been changed.", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), OptionsActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Please login to change your information", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                    else Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+
+                }
         });
     }
 }
