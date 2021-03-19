534cc754ed833f3854a04a01424b4575d2d4e85e
Saif Mahmud <60409889+saif81bd@users.noreply.github.com>
2021-03-19 14:18:10 -0500

    replaced update button with my dashboard button on profile page

--- app/src/main/java/presentation/ProfileActivity.java
+++ app/src/main/java/presentation/ProfileActivity.java
@@ -23,11 +23,13 @@ import objects.User;
 public class ProfileActivity extends AppCompatActivity {
 
     TextInputLayout userName, password;
-    MaterialButton updateButton;
+    MaterialButton dashboardButton;
     User loggedInUser;
     Account account;
-    String message;
+    String message1, message2;
 
+    String userNameFromDB;
+    String passwordFromDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,39 +37,50 @@ public class ProfileActivity extends AppCompatActivity {
 
         userName = findViewById(R.id.profileUserNameField);
         password = findViewById(R.id.profilePasswordField);
-        updateButton = findViewById(R.id.profileUpdateButton);
+        dashboardButton = findViewById(R.id.profileUpdateButton);
 
         loggedInUser = LoggedUser.getLoggedUser();
 
         showAllData();
 
-        account = new Account();
-        updateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                    message = AccountValidator.validatePassword(password.toString());
-                    message = AccountValidator.validateUserName(userName.toString());
+//        account = new Account();
 
-                    if(message == null) {
-                        if (account.changeUser(new User(userName.toString(), password.toString()))) {
-                            Toast.makeText(getApplicationContext(), "Your information has been changed.", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(getApplicationContext(), OptionsActivity.class);
-                            startActivity(intent);
-                        } else {
-                            Toast.makeText(getApplicationContext(), "Please login to change your information", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                            startActivity(intent);
-                        }
-                    } else Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-                }
-        });
     }
+
+
+//        updateButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                    message1 = AccountValidator.validateUserName(userName.toString());
+//                    message2 = AccountValidator.validatePassword(password.toString());
+//
+//                    if(message1 != null && message2 != null) {
+//                        User newUser = new User(userName.toString(), password.toString());
+//
+//                        if (account.changeUser(newUser)) {
+//                            loggedInUser = LoggedUser.getLoggedUser();
+//                            Toast.makeText(getApplicationContext(), "Your information has been changed.", Toast.LENGTH_SHORT).show();
+//                            Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+//                            startActivity(intent);
+//
+//                        } else {
+//                            Toast.makeText(getApplicationContext(), "Please login to change your information", Toast.LENGTH_SHORT).show();
+//                            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+//                            startActivity(intent);
+//                        }
+//                    } else {
+//                        Toast.makeText(getApplicationContext(), message1, Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//        });
+//    }
 
     private void showAllData() {
 
         if(loggedInUser != null){
-            String userNameFromDB = loggedInUser.getUserName();
-            String passwordFromDB = loggedInUser.getPassword();
+            userNameFromDB = loggedInUser.getUserName();
+            passwordFromDB = loggedInUser.getPassword();
 
             userName.getEditText().setText(userNameFromDB);
             password.getEditText().setText(passwordFromDB);
@@ -75,6 +88,11 @@ public class ProfileActivity extends AppCompatActivity {
         }
 
     }
+
+    public void openOptionsActivity(View view){
+        Intent intent_options = new Intent(this, OptionsActivity.class);
+        startActivity(intent_options);
+    }//openOptionsActivity
 
 
 }
