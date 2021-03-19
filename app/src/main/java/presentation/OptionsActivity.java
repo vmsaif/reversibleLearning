ceccef7872dbb272d5f6daae86db85daf891bf5e
Saif Mahmud <60409889+saif81bd@users.noreply.github.com>
2021-03-18 19:55:23 -0500

    fixed app crashing when switching to ProfileActivity.java from OptionsActivity.java and some minor tweaks.

--- app/src/main/java/presentation/OptionsActivity.java
+++ app/src/main/java/presentation/OptionsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
--- app/src/main/java/presentation/ProfileActivity.java
+++ app/src/main/java/presentation/ProfileActivity.java
@@ -2,14 +2,35 @@ package presentation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 import flashcard.group5.application.R;
 import logic.Account;
 
-public class ProfileActivity extends Activity {
+public class ProfileActivity extends AppCompatActivity {
+
+    TextInputLayout userName, password;
+    MaterialButton updateButton;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        userName = findViewById(R.id.profileUserNameField);
+        password = findViewById(R.id.profilePasswordField);
+        updateButton = findViewById(R.id.profileUpdateButton);
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // button things
+            }
+        });
     }
 }
