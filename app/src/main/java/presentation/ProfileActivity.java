b848084880e49cc38381e21a157496eb7dc7a77a
LizaYa <liza.ya3011@gmail.com>
2021-04-14 11:04:32 -0500

    Fix bug updating users

--- app/src/main/java/presentation/ProfileActivity.java
+++ app/src/main/java/presentation/ProfileActivity.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import data.hsqldb.FlashcardPersistenceHSQLDB;
 import flashcard.group5.application.R;
+import logic.Account;
 import logic.FlashcardLogic;
 import logic.LoggedUser;
 import objects.Flashcard;
@@ -28,13 +29,14 @@ public class ProfileActivity extends AppCompatActivity {
     private String userNameFromDB;
     private String passwordFromDB;
     private FlashcardLogic flashcardLogic;
-
+    private Account account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         flashcardLogic = new FlashcardLogic();
+        account = new Account();
 
         userName = findViewById(R.id.profileUserNameField);
         password = findViewById(R.id.profilePasswordField);
@@ -60,6 +62,15 @@ public class ProfileActivity extends AppCompatActivity {
         }//if
     }//showAllData
 
+    public void updateUser(View view){
+        userName = findViewById(R.id.profileUserNameField);
+        password = findViewById(R.id.profilePasswordField);
+        String newUserName = userName.getEditText().getText().toString();
+        String newPassword = password.getEditText().getText().toString();
+        account.changeUser(new User(newUserName,newPassword));
+        Intent intent = new Intent(this, OptionsActivity.class);
+        startActivity(intent);
+    }
 
     //getNumberOfUserCards---returns how many cards a user has
     private String getNumberOfUserCards(){
